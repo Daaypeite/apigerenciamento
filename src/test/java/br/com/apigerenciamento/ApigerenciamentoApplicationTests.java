d1b22298c71c7f653020540bd5478876473fabb0
Daaypeite <daiane.peite@hotmail.com>
2023-01-04 19:41:43 -0300

    First Commit

---
+++ src/test/java/br/com/apigerenciamento/ApigerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apigerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
